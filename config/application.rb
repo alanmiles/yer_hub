1a2401c4cc73d3bac2810dad8486371011cb5b7f
Alan Miles <alanpqs@gmail.com>
2012-04-05 22:58:43 +0100

    revised assets list

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module YerHub
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-    
+    config.assets.precompile += %w( *.js screen.css print.css )
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
