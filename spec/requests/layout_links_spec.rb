7c5aef8f770fbad8cc8f27ecf89967f20c6f1ac9
Alan Miles <alanpqs@gmail.com>
2012-04-08 08:30:35 +0100

    Finish user edit, update, index and destroy actions

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -44,7 +44,7 @@ describe "LayoutLinks" do
     it "should have a signin link" do
       visit root_path
       response.should have_selector("a", :href => signin_path,
-                                         :content => "Sign in")
+                                         :content => "Log in")
     end
   end
 
